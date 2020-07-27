e0b7aa8f00768e0cdf1e6c071072d794044a3c33
YurimLee999 <67811883+YurimLee999@users.noreply.github.com>
2020-07-28 02:30:49 +0900

    08 회원가입 시에 next 인자 처리하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
+from django.conf import settings
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.shortcuts import redirect, render, resolve_url
+from .forms import SignupForm
+'''
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {
+        'form':form,
+    })
+'''
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        next_url = request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+    def form_valid(self,form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
 
 
-post_list = ListView.as_view(model=Post)
+signup = SignupForm.as_view()
 
-post_detail = DetailView.as_view(model=Post)
+@login_required
+def profile(request):
+    request.user
+    return render(request, 'accounts/profile.html')
