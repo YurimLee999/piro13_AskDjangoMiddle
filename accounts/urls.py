6ba1c68619308e625a58fff2b665bd612a9d8db5
YurimLee999 <67811883+YurimLee999@users.noreply.github.com>
2020-07-28 01:17:25 +0900

    06 가입과 동시에 Profile 만들기

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LoginView.as_view(), name='logout')
+    path('logout/', auth_views.LoginView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
