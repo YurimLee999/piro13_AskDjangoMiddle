77c6db5602e8934249af866c708bbb10c2c7f98d
YurimLee999 <67811883+YurimLee999@users.noreply.github.com>
2020-07-28 01:51:57 +0900

    07 회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,8 @@
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            return redirect(settings.LOGIN_URL)
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {
-        'form':form,
-    })
 
-@login_required
-def profile(request):
-    request.user
-    return render(request, 'accounts/profile.html')
+post_list = ListView.as_view(model=Post)
+
+post_detail = DetailView.as_view(model=Post)
